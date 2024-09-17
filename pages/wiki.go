3c5364cce8a80f6f0e3be7ccd8999ddacb97cda8
Hugo Manrique <hugomanriquedesarrollador@gmail.com>
2024-09-17 18:12:04 -0500

    ok

--- pages/wiki.go
+++ pages/wiki.go
@@ -5,39 +5,52 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	return os.WriteFile(filename,p.Body,0644)
+	return os.WriteFile(filename, p.Body, 0644)
 }
 
-func load(title string) (*Page,error){
+func load(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title:title,Body: body}, nil
+	return &Page{Title: title, Body: body}, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"Hola me encantan los %s","Hugo")
+	title := r.URL.Path[len("/view/"):]
+	p, _ := load(title)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	fmt.Printf(title)
+	p, _ := load(title)
 
-func main(){
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func main() {
 	// p1 := &Page{Title: "Hugo", Body: []byte("Hugo is a static site generator written in Go.")}
 	// p1.save()
-	
+
 	// p2, _ := load("Hugo")
 	// fmt.Println(string(p2.Body))
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil) )
+	http.HandleFunc("/view/", handler)
+	http.HandleFunc("/edit/", editHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
