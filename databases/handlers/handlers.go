e40be852fb8000d988570eece3e6a123c1424d4c
Hugo Manrique <hugomanriquedesarrollador@gmail.com>
2024-10-03 14:45:35 -0500

    ok

--- databases/handlers/handlers.go
+++ databases/handlers/handlers.go
@@ -32,3 +32,50 @@ func ListContacts(db *sql.DB) {
 		fmt.Println(contact)
 	}
 }
+
+func GetContactById(db *sql.DB, id int) {
+	query := "SELECT * FROM contacts WHERE id = ?"
+	row := db.QueryRow(query, id)
+	var contact models.Contact
+	var valueMail sql.NullString
+	err := row.Scan(&contact.Id, &contact.Name, &valueMail, &contact.Phone)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatal("Id not found")
+		}
+		log.Fatal(err)
+	}
+	if valueMail.Valid {
+		contact.Email = valueMail.String
+	} else {
+		contact.Email = "Sin correo"
+	}
+	log.Println(contact)
+}
+
+func CreateContact(db *sql.DB, contact models.Contact) {
+	query := "INSERT INTO contacts (name, email, phone) VALUES (?, ?, ?)"
+	_, err := db.Exec(query, contact.Name, contact.Email, contact.Phone)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Contact created")
+}
+
+func UpdateContact(db *sql.DB, contact models.Contact) {
+	query := "UPDATE contacts SET name = ?, email = ?, phone = ? WHERE id = ?"
+	_, err := db.Exec(query, contact.Name, contact.Email, contact.Phone, contact.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Contact updated")
+}
+
+func DeleteContact(db *sql.DB, id int) {
+	query := "DELETE FROM contacts WHERE id = ?"
+	_, err := db.Exec(query, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Contact deleted")
+}
--- databases/main.go
+++ databases/main.go
@@ -15,4 +15,16 @@ func main() {
 	}
 	defer db.Close()
 	handlers.ListContacts(db)
+	handlers.GetContactById(db, 1)
+
+	// newContact := models.Contact{
+	// 	Id:    2,
+	// 	Name:  "Modificado",
+	// 	Email: "modificado@gmail.com",
+	// 	Phone: "9992223333",
+	// }
+	// handlers.UpdateContact(db, newContact)
+	// handlers.CreateContact(db, newContact)
+	handlers.DeleteContact(db, 4)
+	handlers.ListContacts(db)
 }
