e5f07b3ea875318b5daebeddab40cd8004a41fec
hugomanrique <hugomanriquedesarrollador@gmail.com>
2024-10-03 21:07:58 -0500

    ok

--- rpsweb/handlers/handlers.go
+++ rpsweb/handlers/handlers.go
@@ -2,11 +2,21 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
+	tpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
