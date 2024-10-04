3251a41c645cb2b169bec0c8fca017ef8bc8e58f
Hugo Manrique <hugomanriquedesarrollador@gmail.com>
2024-10-04 18:02:35 -0500

    ok

--- rpsweb/handlers/handlers.go
+++ rpsweb/handlers/handlers.go
@@ -3,39 +3,58 @@ package handlers
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+const (
+	templateDir  = "templates/"
+	templateBase = templateDir + "base.html"
+)
+
+func renderTemplate(w http.ResponseWriter, page string, data any) {
+	tpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	tpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
-	data := struct {
-		Title   string
-		Message string
-	}{
-		Title:   "Pagina de inicio",
-		Message: "Bienvenido a nuestra pagina de inicio",
-	}
+	renderTemplate(w, "index.html", nil)
+	// tpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
-	err = tpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// data := struct {
+	// 	Title   string
+	// 	Message string
+	// }{
+	// 	Title:   "Pagina de inicio",
+	// 	Message: "Bienvenido a nuestra pagina de inicio",
+	// }
+
+	// err = tpl.Execute(w, data)
+
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "New Game")
+	renderTemplate(w, "new-game.html", nil)
 }
 
 func Game(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Game")
+	renderTemplate(w, "game.html", nil)
 }
 
 func Play(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Play")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html", nil)
 }
--- rpsweb/main.go
+++ rpsweb/main.go
@@ -10,10 +10,15 @@ func main() {
 
 	router := http.NewServeMux()
 
+	// Manage static files
+	fs := http.FileServer(http.Dir("static"))
+	router.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	router.HandleFunc("/", handlers.Index)
 	router.HandleFunc("/new", handlers.NewGame)
 	router.HandleFunc("/game", handlers.Game)
 	router.HandleFunc("/play", handlers.Play)
+	router.HandleFunc("/about", handlers.About)
 	port := "8080"
 	fmt.Println("Serving on port", port)
 	http.ListenAndServe(":"+port, router)
